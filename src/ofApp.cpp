8ececcee2e41122693862102b6436cbc7ca00250
JAGormley <andrewjgormley@gmail.com>
2015-05-21 00:37:52 -0700

    init

---
+++ src/ofApp.cpp
@@ -0,0 +1,84 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    
+    shader.load("shadersGL3/shader");
+    
+    float planeScale = 0.75;
+    int planeWidth = ofGetWidth() * planeScale;
+    int planeHeight = ofGetHeight() * planeScale;
+    int planeGridSize = 20;
+    int planeColums = planeWidth / planeGridSize;
+    int planeRows = planeHeight / planeGridSize;
+    
+    plane.set(planeWidth, planeHeight, planeColums, planeRows, OF_PRIMITIVE_TRIANGLES);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    
+    // translate plane into center screen.
+    float tx = ofGetWidth() / 2;
+    float ty = ofGetHeight() / 2;
+    ofTranslate(tx, ty);
+    
+    // the mouse/touch Y position changes the rotation of the plane.
+    float percentY = mouseY / (float)ofGetHeight();
+    float rotation = ofMap(percentY, 0, 1, -60, 60, true) + 60;
+    ofRotate(rotation, 1, 0, 0);
+    
+    plane.drawWireframe();
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
